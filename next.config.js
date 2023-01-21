1cc9c6a94d490c846f607a99fe167ef7f99137dd
luangelsdorf <luanferreira2136@gmail.com>
2023-01-20 21:05:01 -0300

    Added svgr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+
+  webpack(config) {
+    config.modules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+            replaceAttrValues: { 'white': 'currentColor' }
+          }
+        }
+      ],
+    })
+  }
 }
 
 module.exports = nextConfig
