dd2b1c2215e678e5727c4d45cb7e22bffb80c9d7
luangelsdorf <luanferreira2136@gmail.com>
2023-01-24 08:24:15 -0300

    Home Contact section

---
+++ src/components/home/Contact/index.js
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './Contact.module.scss';
+import ArrowLarge from 'public/images/icons/ArrowLarge.svg';
+
+export default function Contact({ content }) {
+  return (
+    <div className={styles.section}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-10 offset-lg-1">
+            <div className={styles.learnMore}>
+              <h2>{'Vamos Iniciar \nseu Projeto'}</h2>
+              <p>Quer saber mais sobre como podemos lhe ajudar ou sanar alguma dúvida? <strong>Marque uma reunião</strong>, será um prazer lhe atender.</p>
+              <Link href="#" className={styles.circleButton}>
+                <ArrowLarge />
+              </Link>
+            </div>
+            <div className={styles.links}>
+              <div>
+                <h3>Contatos</h3>
+                <address>
+                  <a href="tel:+5551989836186">+55 51 98983.6186</a>
+                  <a href="mailto:contact@brstorm.design">contact@brstorm.design</a>
+                </address>
+              </div>
+              <div>
+                <h3>Brand Design</h3>
+                <Link href="#">{'Sua Marca Muito \nAlém do Visual'}</Link>
+              </div>
+              <div>
+                <h3>Digital Design</h3>
+                <Link href="#">{'Tenha uma Vitrine \n24/7 do seu Negócio'}</Link>
+              </div>
+              <div>
+                <h3>Outros Links</h3>
+                <Link href="#">Termos de Uso</Link>
+                <Link href="#">Política de Privacidade</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
