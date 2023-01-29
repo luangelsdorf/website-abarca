ff58d119911980214eecd74cd1194d8033a6bb6e
luangelsdorf <luanferreira2136@gmail.com>
2023-01-29 14:40:34 -0300

    Form sending utils

---
+++ src/utils/getQueryString.js
@@ -0,0 +1,20 @@
+export default (object) => {
+  const data = { ...object };
+  const params = new URLSearchParams();
+
+  for (let key in data) {
+    let value = data[key];
+    if (key.includes('entry')) {
+      if (typeof value === 'object') {
+        for (let nestedKey in value) {
+          let nestedValue = value[nestedKey];
+          if (nestedValue) params.append(key, nestedKey);
+        }
+      } else {
+        params.append(key.replace('_', '.'), value);
+      }
+    }
+  }
+
+  return params;
+}
---
+++ src/utils/submitForm.js
@@ -0,0 +1,19 @@
+import getQueryString from "./getQueryString";
+
+export default async (data, event) => {
+  const fields = Array.from(event.target.elements);
+  fields.forEach(field => field.disabled = true);
+
+  /* const request = await fetch('https://docs.google.com/forms/u/1/d/e/1FAIpQLSfzqZq8vFpdno3w49wMUTZqEvw-rQUZLTqJpzdGITNaeOUj6w/formResponse', {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: getQueryString(data),
+  });
+
+  if (request) {
+    router.push('/contact/success');
+  } else {
+    window.location.reload();
+  } */
+}
