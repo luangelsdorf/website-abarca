405159703887d80036b697d154203ea09eb60604
luangelsdorf <luanferreira2136@gmail.com>
2023-02-02 17:20:58 -0300

    Hotfix

--- src/pages/structures.js
+++ src/pages/structures.js
@@ -33,7 +33,7 @@ export default function Structures() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip commodo.
             <br />
             <br />
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </Intro>
         </Section>
 
