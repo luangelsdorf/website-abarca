f644d6832f332d307f13e8eb8469be0826cd3de2
luangelsdorf <luanferreira2136@gmail.com>
2023-02-03 08:32:51 -0300

    Portfolio Abarca

--- src/components/portfolio/ProjectPresentation/index.js
+++ src/components/portfolio/ProjectPresentation/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './ProjectPresentation.module.scss';
 
-export default function ProjectPresentation({ children }) {
+export default function ProjectPresentation({ children, style }) {
   return (
-    <article className={styles.presentation}>
+    <article className={styles.presentation} style={style}>
       {
         children
       }
