3a57d7e5ba83a5376e71fa0d09d2b14bf85ee3d7
luangelsdorf <luanferreira2136@gmail.com>
2023-02-03 20:42:53 -0300

    Fixed portfolio images

--- src/components/portfolio/structures/PictureAndText/index.js
+++ src/components/portfolio/structures/PictureAndText/index.js
@@ -5,14 +5,18 @@ export default function PictureAndText({ title, flipped, children }) {
   let [first, ...rest] = children;
   return (
     <div className={`${styles.section}${flipped ? ' ' + styles.flipped : ''}`}>
-      <div className="container">
-        <div className="row justify-content-between">
-          <div className="col-12 col-lg-6" /* style={{ order: flipped ? 1 : 0 }} */>
+      <div>
+        <div className={`row justify-content-${flipped ? 'end' : 'start'}`}>
+          <div className="col-12 col-lg-6">
             <div className={styles.first}>
               {first}
             </div>
           </div>
-          <div className="col-12 col-lg-5" /* style={{ order: flipped ? 0 : 1 }} */>
+        </div>
+      </div>
+      <div className="container">
+        <div className={`row justify-content-${flipped ? 'start' : 'end'}`}>
+          <div className="col-12 col-lg-5">
             <div className={styles.last}>
               <h2>
                 {
