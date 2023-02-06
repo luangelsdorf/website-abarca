829372416f8c9d019cc6b0ef3b560505fe5319df
luangelsdorf <luanferreira2136@gmail.com>
2023-02-06 07:16:51 -0300

    Added Google Tag Manager

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { isDev } from 'src/utils/env'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt">
       <Head>
+        {
+          isDev ? null : (
+            <>
+              <GoogleTagManager />
+            </>
+          )
+        }
         <link rel="shortcut icon" href="/images/icons/favicon.svg" />
         <meta name="description" content="A Br.Storm é um estúdio de design especializado na criação e perpetuação de marcas e produtos digitais." />
       </Head>
