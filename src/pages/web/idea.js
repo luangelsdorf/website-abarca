2eba51c7b8bd1aa0948d5f124edaf769b24e4898
luangelsdorf <luanferreira2136@gmail.com>
2023-04-03 20:54:47 -0300

    Created Digital Design landing pages

---
+++ src/pages/web/idea.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Idea() {
+  return (
+    <div className="container">
+      <div style={{paddingTop: '160px'}}>
+        <h1>Digital Design LP | Idea</h1>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/web/remember.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Remember() {
+  return (
+    <div style={{ paddingTop: '160px' }}>
+      <div className="container">
+        <h1>Digital Design LP | Remember</h1>
+      </div>
+    </div>
+  )
+}
