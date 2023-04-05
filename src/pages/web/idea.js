3cdd3f6aca9f39f6c97cd8835461f463d3c493da
luangelsdorf <luanferreira2136@gmail.com>
2023-04-05 15:53:01 -0300

    Landing page layout changes

--- src/pages/web/idea.js
+++ src/pages/web/idea.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import Head from 'next/head';
+import React from 'react';
+import FAQ from 'src/components/common/FAQ';
+import Section from 'src/components/common/Section';
+import Projects from 'src/components/landing-pages/brand-b/Projects';
+import Contact from 'src/components/landing-pages/brand/Contact';
+import Testimonials from 'src/components/landing-pages/brand/Testimonials';
+import Hero from 'src/components/landing-pages/web/idea/Hero';
+import Footer from 'src/components/layout/Footer';
+import styles from 'src/styles/pages/landingpages.module.scss';
 
 export default function Idea() {
   return (
-    <div className="container">
-      <div style={{paddingTop: '160px'}}>
-        <h1>Digital Design LP | Idea</h1>
-      </div>
+    <div className={styles.page}>
+      <Head>
+        <title>Title | Br.Storm</title>
+        <meta property="og:title" content="Deixe sua Marca no Mundo | Br.Storm" />
+        <meta name="description" content="A Br.Storm é um estúdio de design especializado na criação e perpetuação de marcas e produtos digitais." />
+        <meta property="og:description" content="A Br.Storm é um estúdio de design especializado na criação e perpetuação de marcas e produtos digitais." />
+        <meta property="og:image" content="https://brstorm.design/images/preview.jpg" />
+        <meta property="og:url" content="https://web.brstorm.design" />
+      </Head>
+
+      <main>
+        <Section>
+          <Hero />
+        </Section>
+
+        <Section pt="96" pb="80">
+          <Projects />
+        </Section>
+
+        <Section pt="120" pb="120">
+          <Testimonials />
+        </Section>
+
+        <Section id="faq" pt="120" pb="144">
+          <FAQ />
+        </Section>
+
+        <Section id="contact" pt="120 96" pb="160 0">
+          <Contact />
+        </Section>
+      </main>
+
+      <Footer />
     </div>
   )
 }
--- src/pages/web/remember.js
+++ src/pages/web/remember.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import Section from 'src/components/common/Section';
+import Hero from 'src/components/landing-pages/brand/Hero';
+import Projects from 'src/components/landing-pages/web/common/Projects';
+import styles from 'src/styles/pages/landingpages.module.scss';
 
 export default function Remember() {
   return (
-    <div style={{ paddingTop: '160px' }}>
-      <div className="container">
-        <h1>Digital Design LP | Remember</h1>
-      </div>
+    <div className={styles.page}>
+      <Head>
+        <title>Title | Br.Storm</title>
+        <meta property="og:title" content="Deixe sua Marca no Mundo | Br.Storm" />
+        <meta name="description" content="A Br.Storm é um estúdio de design especializado na criação e perpetuação de marcas e produtos digitais." />
+        <meta property="og:description" content="A Br.Storm é um estúdio de design especializado na criação e perpetuação de marcas e produtos digitais." />
+        <meta property="og:image" content="https://brstorm.design/images/preview.jpg" />
+        <meta property="og:url" content="https://web.brstorm.design" />
+      </Head>
+
+      <main>
+        <Section>
+          <Hero
+            web
+            slot01={<Image data-project="rodrigo-carvalho" fill src="/images/hero/rodrigo-carvalho/cover.jpg" alt="" />}
+            slot02={<Image data-project="grana" fill src="/images/hero/grana/cover.jpg" alt="" />}
+            slot03={<Image data-project="rd-advogados" fill src="/images/hero/rd-advogados/cover.jpg" alt="" />}
+            slot04={<Image data-project="certivale" fill src="/images/hero/certivale/cover.jpg" alt="" />}
+            slot05={<Image data-project="nefrosinos" fill src="/images/hero/nefrosinos/cover.jpg" alt="" />}
+            slot06={<Image data-project="arca" fill src="/images/hero/arca/cover.jpg" alt="" />}
+            slot07={<Image data-project="pegada" fill src="/images/hero/pegada/cover.jpg" alt="" />}
+            slot08={<Image data-project="brc-advogados" fill src="/images/hero/brc-advogados/cover.jpg" alt="" />}
+            slot09={<Image data-project="robtec" fill src="/images/hero/robtec/cover.jpg" alt="" />}
+            slot10={<Image data-project="util" fill src="/images/hero/util/cover.jpg" alt="" />}
+          >
+            <h1>
+              <span className="h-sans">Quem não é visto,</span>
+              <br />
+              <span>não é lembrado!</span>
+            </h1>
+          </Hero>
+        </Section>
+
+        <Section pt="160" pb="88">
+          <Projects />
+        </Section>
+      </main>
     </div>
   )
 }
