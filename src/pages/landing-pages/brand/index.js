aa6f39826ae9b498f033ed52ad34cb2104d4402a
luangelsdorf <luanferreira2136@gmail.com>
2023-04-12 14:14:03 -0300

    Landing pages structure changes

---
+++ src/pages/landing-pages/brand/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BrandLandingA() {
+  return (
+    <h1 style={{marginTop: '160px'}}>Brand Landing Page A</h1>
+  )
+}
---
+++ src/pages/landing-pages/brand/pricing.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BrandLandingB() {
+  return (
+    <h1 style={{marginTop: '160px'}}>Brand Landing Page B (pricing)</h1>
+  )
+}
