a9a2b6109d3bcb3425b670b9806e90909603e840
luangelsdorf <luanferreira2136@gmail.com>
2023-05-28 22:20:33 -0300

    Mobile section fix

--- src/components/landing-pages/web/idea/Mobile/index.js
+++ src/components/landing-pages/web/idea/Mobile/index.js
@@ -42,41 +42,41 @@ export default function Mobile({ content }) {
     <div className={styles.section}>
       <div className="container">
         <div className={styles.outer}>
-          <div className={styles.inner}>
-            <Image src={iphone} alt="" width="519" height="665" />
-            <div className={styles.textContent}>
-              <h2><span className="h-sans">Design que</span> cabe no bolso!</h2>
-              <p>{'Por que esperar para ter um site incrível, vamos começar a criar seu \nsite hoje mesmo? Preencha o formulário abaixo e entre em contato agora!'}</p>
-              <form onSubmit={handleSubmit(submitForm)}>
-                <div className={`floating${!errors.entry_443452100 ? '' : ' ' + 'error'}`}>
-                  <input id={`name_${formId}`} placeholder="Como podemos lhe chamar?" type="text" {...register('entry_443452100', { required: errorMessage })} />
-                  <label htmlFor={`name_${formId}`}>Nome</label>
-                  <span>{errors.entry_443452100 && errorMessage}</span>
-                </div>
-
-                <div className={`floating${!errors.entry_2104795631 ? '' : ' ' + 'error'}`}>
-                  <input id={`business_${formId}`} placeholder="Qual o nome do seu negócio" type="text" {...register('entry_2104795631', { required: errorMessage })} />
-                  <label htmlFor={`business_${formId}`}>Empresa</label>
-                  <span>{errors.entry_2104795631 && errorMessage}</span>
-                </div>
-
-                <div className={`floating${!errors.entry_1354793341 ? '' : ' ' + 'error'} prefixed`}>
-                  <input id={`phone_${formId}`} placeholder="(51) 98983.6186" type="tel" {...register('entry_1354793341', { required: errorMessage, minLength: 14, onChange: e => onChange(e, setValue) })} />
-                  <label htmlFor={`phone_${formId}`}>Telefone</label>
-                  <span className="prefix">+55</span>
-                  <span>{errors.entry_1354793341 && errorMessage}</span>
-                </div>
-
-                <div className={`floating${!errors.entry_1305055579 ? '' : ' ' + 'error'}`}>
-                  <input id={`email_${formId}`} placeholder="contato@brstorm.design" type="email" {...register('entry_1305055579', { required: errorMessage })} />
-                  <label htmlFor={`email_${formId}`}>E-mail</label>
-                  <span>{errors.entry_1305055579 && errorMessage}</span>
-                </div>
-
-                <Button id="form-submit" btnElement type="submit" className="lg form-submit" RightIcon={Arrow}>Enviar Solicitação</Button>
-                <Spinner style={{ display: isSubmitting ? 'block' : 'none' }} />
-                <span style={{ opacity: isSubmitSuccessful ? '1' : '0' }}>✓ Enviada com sucesso!</span>
-              </form>
+          <div className={`row ${styles.inner}`}>
+            <div className="col-12 col-lg-5">
+              <Image src={iphone} alt="" width="519" height="665" />
+            </div>
+            <div className="col-12 col-lg-7">
+              <div className={styles.textContent}>
+                <h2><span className="h-sans">Design que</span> cabe no bolso!</h2>
+                <p>{'Por que esperar para ter um site incrível, vamos começar a criar seu \nsite hoje mesmo? Preencha o formulário abaixo e entre em contato agora!'}</p>
+                <form onSubmit={handleSubmit(submitForm)}>
+                  <div className={`floating${!errors.entry_443452100 ? '' : ' ' + 'error'}`}>
+                    <input id={`name_${formId}`} placeholder="Como podemos lhe chamar?" type="text" {...register('entry_443452100', { required: errorMessage })} />
+                    <label htmlFor={`name_${formId}`}>Nome</label>
+                    <span>{errors.entry_443452100 && errorMessage}</span>
+                  </div>
+                  <div className={`floating${!errors.entry_2104795631 ? '' : ' ' + 'error'}`}>
+                    <input id={`business_${formId}`} placeholder="Qual o nome do seu negócio" type="text" {...register('entry_2104795631', { required: errorMessage })} />
+                    <label htmlFor={`business_${formId}`}>Empresa</label>
+                    <span>{errors.entry_2104795631 && errorMessage}</span>
+                  </div>
+                  <div className={`floating${!errors.entry_1354793341 ? '' : ' ' + 'error'} prefixed`}>
+                    <input id={`phone_${formId}`} placeholder="(51) 98983.6186" type="tel" {...register('entry_1354793341', { required: errorMessage, minLength: 14, onChange: e => onChange(e, setValue) })} />
+                    <label htmlFor={`phone_${formId}`}>Telefone</label>
+                    <span className="prefix">+55</span>
+                    <span>{errors.entry_1354793341 && errorMessage}</span>
+                  </div>
+                  <div className={`floating${!errors.entry_1305055579 ? '' : ' ' + 'error'}`}>
+                    <input id={`email_${formId}`} placeholder="contato@brstorm.design" type="email" {...register('entry_1305055579', { required: errorMessage })} />
+                    <label htmlFor={`email_${formId}`}>E-mail</label>
+                    <span>{errors.entry_1305055579 && errorMessage}</span>
+                  </div>
+                  <Button id="form-submit" btnElement type="submit" className="lg form-submit" RightIcon={Arrow}>Enviar Solicitação</Button>
+                  <Spinner style={{ display: isSubmitting ? 'block' : 'none' }} />
+                  <span style={{ opacity: isSubmitSuccessful ? '1' : '0' }}>✓ Enviada com sucesso!</span>
+                </form>
+              </div>
             </div>
           </div>
         </div>
