3f4e69e241de2c43f001e3ebb7feafcfd861de7e
luangelsdorf <luanferreira2136@gmail.com>
2025-08-01 07:31:54 -0300

    Redirect home page to discount page

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
 
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/lp/website/desconto',
+      },
+    ]
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
